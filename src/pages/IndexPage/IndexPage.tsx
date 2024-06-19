e1898e898e1e39417ffbcc2a845c553f64ff3860
petrakovpk <petrakovpk@gmail.com>
2024-06-19 13:15:01 +0300

    weak commit

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -14,7 +14,7 @@ export const IndexPage: Component = () => {
   });
 
   return (
-    <Page title="Home Page">
+    <Page title="Home Page PASHA">
       <p>
         This page is a home page in this boilerplate. You can use the links below to visit other
         pages with their own functionality.
